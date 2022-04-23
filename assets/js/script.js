c4eea6d39639e60b6073e8d08b31fd30f9b36ccb
rstanbaugh <rich_bootcamp@icloud.com>
2022-04-23 19:31:09 -0400

    form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 // variables declarations
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 
 
-
+var createTaskHandler = function(event) {
+  event.preventDefault();
+  
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task";
+  tasksToDoEl.appendChild(listItemEl);
+}
 
 
 
@@ -13,9 +20,4 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 // event listeners
-buttonEl.addEventListener("click", function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "this is a new task";
-  tasksToDoEl.appendChild(listItemEl);
-})
+formEl.addEventListener("submit", createTaskHandler);
