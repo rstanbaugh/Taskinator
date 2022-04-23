55a32e0b559bae0c5cdb89dc84748f4c92dc79f7
rstanbaugh <rich_bootcamp@icloud.com>
2022-04-23 18:18:19 -0400

    add ability to add a task

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// variables declarations
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+
+
+
+
+
+
+
+
+
+// event listeners
+buttonEl.addEventListener("click", function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "this is a new task";
+  tasksToDoEl.appendChild(listItemEl);
+})
